ae6e80d8388b45c5ecb4e88a590c1a2956ec0598
Kevin Ricardino <kevin.j.bernardes@gmail.com>
2026-02-08 22:06:33 -0300

    Adicionar recurso de compra e venda

--- bot_bitcoin.py
+++ bot_bitcoin.py
@@ -3,13 +3,27 @@ import json
 from datetime import datetime, timedelta, timezone
 
 import websocket
+import bitstamp.client
 
-def comprar():
-    pass
+import credenciais
 
 
-def vender():
-    pass
+def cliente():
+    return bitstamp.client.Trading(
+        username =credenciais.USERNAME,
+        key =credenciais.KEY,
+        secret =credenciais.SECRET
+    )
+
+
+def comprar(quantidade):
+    trading_client = cliente()
+    trading_client.buy_market_order(quantidade)
+
+
+def vender(quantidade):
+    trading_client = cliente()
+    trading_client.sell_market_order(quantidade)
 
 
 def ao_abrir(ws):
